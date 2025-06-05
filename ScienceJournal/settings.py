e8204b0e69740cf8fafc44c791a50c6b6f63392e
Erasil <erasil.bakhytgan@gmail.com>
2025-06-05 14:34:50 +0500

    debug = true

--- ScienceJournal/settings.py
+++ ScienceJournal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7clcqx9fza*&4o^8v(fg2o%gx_2p5@(-mvk9e3q=azwn^++vtf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # Application definition
