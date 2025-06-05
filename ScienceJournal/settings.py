c23d38d931ede9d5544c267f24a56651a51eebae
Erasil <erasil.bakhytgan@gmail.com>
2025-06-05 14:44:56 +0500

    update settings

--- ScienceJournal/settings.py
+++ ScienceJournal/settings.py
@@ -133,11 +133,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Указывает, где искать статику во время разработки
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
